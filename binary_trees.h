62a5ce029f45ab04b3b09c509f292914fb72fbcf
riikakelvin <riikakelvin2@gmail.com>
2023-08-31 12:41:45 -0700

    commit for task 18, 18. Uncle, a function that finds the uncle of a node

--- binary_trees.h
+++ binary_trees.h
@@ -55,6 +55,12 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int is_perfect_recursive(const binary_tree_t *tree,
+		size_t leaf_depth, size_t level);
+const binary_tree_t *get_leaf(const binary_tree_t *tree);
+size_t depth(const binary_tree_t *tree);
+unsigned char is_leaf(const binary_tree_t *node);
 
 /* Advanced tasks */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
